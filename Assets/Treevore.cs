984cd5fa935cda3e79c69b5f76010f2b638cf282
VixYW <vinicius.g.figueiro@gmail.com>
2019-01-26 02:18:17 -0200

    Arvore funcionando.

--- Assets/Treevore.cs
+++ Assets/Treevore.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tree : MonoBehaviour
+public class Treevore : MonoBehaviour
 {
     public GameObject drop;
 
     public int amount;
 
     void OnTriggerEnter2D(Collider2D col){
+        Debug.Log("Colidiu");
         if (col.tag == "Boom"){
             Debug.Log("Arvore Imortal eh o krl!!!");
             while (amount > 0){
