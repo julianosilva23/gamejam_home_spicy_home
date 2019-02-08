644f70f518b6082550c80b393cae31ce7059b08b
VixYW <vinicius.g.figueiro@gmail.com>
2019-02-08 17:33:01 -0200

    Cursor no menu.

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+
+    void Update(){
+
+    gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(
+
+        28000 * Time.fixedDeltaTime * Input.GetAxisRaw("Horizontal"),
+
+        28000 * Time.fixedDeltaTime * Input.GetAxisRaw("Vertical"));
+    }
+}
