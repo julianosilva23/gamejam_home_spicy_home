cc257649d9ddd57fcba14e981366e8ca94c9a101
VixYW <vinicius.g.figueiro@gmail.com>
2019-02-15 23:42:24 -0200

    Menus aprimorados. Não consegui fazer funcionar a mudança da cor do botão quando o personagem é selecionado.

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -6,23 +6,21 @@ using UnityEngine.UI;
 public class Cursor : MonoBehaviour
 {
     Button target;
-    GameObject targetChar;
-    GameObject selectedChar;
+    public GameObject targetChar;
+    public GameObject selectedChar;
     bool onButton;
     bool onChar;
 
+    public MenuNew menuManager;
     public string typeInput;
     public int playerNumber;
     public Color playerColor;
-
-    MenuNew menuManager;
 
     void Start(){
         onButton = false;
         target = null;
         targetChar = null;
         selectedChar = null;
-        menuManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<MenuNew>();
     }
 
     void FixedUpdate(){
@@ -30,21 +28,27 @@ public class Cursor : MonoBehaviour
             280 * Time.fixedDeltaTime * Input.GetAxisRaw("Horizontal" + typeInput),
             280 * Time.fixedDeltaTime * Input.GetAxisRaw("Vertical" + typeInput)
         );
-        if (Input.GetButtonDown("Submit") && onButton){
+        if ((Input.GetButtonDown("Submit") || Input.GetButtonDown("Fire1" + typeInput)) && onButton){
             target.onClick.Invoke();
         }
-        if (Input.GetButtonDown("Submit") && onChar){
+        if (Input.GetButtonDown("Fire1" + typeInput) && onChar){
             if (!targetChar.GetComponent<CharButton>().isSelected){
                 if (selectedChar != null){
                     selectedChar.GetComponent<Image>().color = new Color (0,0,0);
-                    targetChar.GetComponent<CharButton>().isSelected = false;
+                    selectedChar.GetComponent<CharButton>().isSelected = false;
+                    selectedChar.GetComponent<Button>().interactable = true;
+                    Debug.Log("pt2");
                 } else {
                     menuManager.playersReady ++;
+                    Debug.Log("pt3");
                 }
                 selectedChar = targetChar;
-                selectedChar.GetComponent<Image>().color = playerColor;
-                targetChar.GetComponent<CharButton>().isSelected = true;
+                selectedChar.GetComponent<Image>().color = Color.red;
+                selectedChar.GetComponent<Button>().interactable = true;
+                selectedChar.GetComponent<CharButton>().isSelected = true;
                 Keyboard.NumberChar[playerNumber] = targetChar.GetComponent<CharButton>().charNumber;
+                Debug.Log(selectedChar.GetComponent<Image>().color.ToString());
+                Debug.Log(playerColor.ToString());
             }
         }
     }
@@ -57,6 +61,7 @@ public class Cursor : MonoBehaviour
         }
         if (col.tag == "CharButton"){
             targetChar = col.gameObject;
+            //targetChar.GetComponent<Button>().Select();
             onChar = true;
         }
     }
