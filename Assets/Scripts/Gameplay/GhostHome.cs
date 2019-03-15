ee44e338432ea4576989b17163a00f6b231c364a
VixYW <vinicius.g.figueiro@gmail.com>
2019-03-15 14:37:56 -0300

    Torna-se possível novamente prender o oponente na sua casa sem precisar bombardear o infeliz.

--- Assets/Scripts/Gameplay/GhostHome.cs
+++ Assets/Scripts/Gameplay/GhostHome.cs
@@ -9,7 +9,7 @@ public class GhostHome : MonoBehaviour
     public bool canBuild;
 
     public List<GameObject> obstacles;
-    public List<GameObject> playerObstacles;
+    //public List<GameObject> playerObstacles;
 
     void Start(){
        canBuild = true;
@@ -25,13 +25,13 @@ public class GhostHome : MonoBehaviour
 
     void CheckObstacles(){
         if (canBuild){
-            if (obstacles.Count > 0 || CheckPlayerObstacles()){
+            if (obstacles.Count > 0){         //|| CheckPlayerObstacles()
                 canBuild = false;
                 GetComponent<SpriteRenderer>().color = new Color32(255, 29, 0, 130);
                 //Debug.Log("Mudou p/ vermelho");
                 //forbiddenImage.SetActive(true);
             }
-        } else if (obstacles.Count == 0 && !CheckPlayerObstacles()) {
+        } else if (obstacles.Count == 0) {         // && !CheckPlayerObstacles()
             canBuild = true;
             GetComponent<SpriteRenderer>().color = new Color32(0, 175, 255, 130);
             //Debug.Log("Mudou p/ azul");
@@ -39,7 +39,7 @@ public class GhostHome : MonoBehaviour
         }
     }
 
-    bool CheckPlayerObstacles(){
+    /*bool CheckPlayerObstacles(){
         if (playerObstacles.Count > 0){
             foreach (GameObject player in playerObstacles){
                 if (player.GetComponent<Player>().alive){
@@ -48,7 +48,7 @@ public class GhostHome : MonoBehaviour
             }
         }
         return false;
-    }
+    }*/
 
     void OnTriggerEnter2D(Collider2D col){
         switch (ValidCollision(col)){
@@ -57,9 +57,9 @@ public class GhostHome : MonoBehaviour
             case 1:
                 obstacles.Add(col.gameObject);
                 return;
-            case 2:
-                playerObstacles.Add(col.gameObject);
-                return;
+            //case 2:
+            //    playerObstacles.Add(col.gameObject);
+            //    return;
         }
     }
 
@@ -70,9 +70,9 @@ public class GhostHome : MonoBehaviour
             case 1:
                 obstacles.Remove(col.gameObject);
                 return;
-            case 2:
-                playerObstacles.Remove(col.gameObject);
-                return;
+            //case 2:
+            //    playerObstacles.Remove(col.gameObject);
+            //    return;
         }
     }
 
